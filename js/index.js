0e64d39309b81f38c2af94b5b6332025e198ae21
anuraghazra <hazru.anurag@gmail.com>
2018-09-01 20:10:17 +0530

    added window.onload

--- js/index.js
+++ js/index.js
@@ -2,52 +2,57 @@
  * MSW LOGO Interpeter
  * @author AnuragHazra <hazru.anurag@gmail.com>
  */
-let canvas = document.getElementById('c');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - 150;
-const c = new Candy(canvas);
-c.smooth('high')
 
-let w = CANVAS_WIDTH;
-let h = CANVAS_HEIGHT;
+window.onload = function () {
 
-let inputCommand = document.getElementById('commands');
-let historyConsole = document.getElementById('history');
-let executeBtn = document.getElementById('exeBtn');
+  let canvas = document.getElementById('c');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - 150;
+  const c = new Candy(canvas);
+  c.smooth('high')
 
-executeBtn.onclick = function () {
-  historyConsole.scrollTop += 100;
-  turtle.move(inputCommand.value);
-  historyConsole.innerHTML += '<p>'+inputCommand.value + '</p>';
-  inputCommand.value = '';
-  inputCommand.select();
-}
+  let w = CANVAS_WIDTH;
+  let h = CANVAS_HEIGHT;
 
+  let inputCommand = document.getElementById('commands');
+  let historyConsole = document.getElementById('history');
+  let executeBtn = document.getElementById('exeBtn');
 
-window.addEventListener('keydown', function (e) {
-  historyConsole.scrollTop += 100;
-  if (e.keyCode === 13) {
+  executeBtn.onclick = function () {
+    historyConsole.scrollTop += 100;
     turtle.move(inputCommand.value);
-    historyConsole.innerHTML += '<p>'+inputCommand.value + '</p>';
+    historyConsole.innerHTML += '<p>' + inputCommand.value + '</p>';
     inputCommand.value = '';
     inputCommand.select();
   }
-});
 
-historyConsole.addEventListener('click', function(e) {
-  if (e.target.tagName === 'P') {
-    console.log(e.target)
-    inputCommand.value = e.target.innerHTML;
-    inputCommand.select();
+
+  window.addEventListener('keydown', function (e) {
+    historyConsole.scrollTop += 100;
+    if (e.keyCode === 13) {
+      turtle.move(inputCommand.value);
+      historyConsole.innerHTML += '<p>' + inputCommand.value + '</p>';
+      inputCommand.value = '';
+      inputCommand.select();
+    }
+  });
+
+  historyConsole.addEventListener('click', function (e) {
+    if (e.target.tagName === 'P') {
+      console.log(e.target)
+      inputCommand.value = e.target.innerHTML;
+      inputCommand.select();
+    }
+  })
+
+  const turtle = new Turtle();
+
+  function animate() {
+    c.clear();
+
+    turtle.render();
+
+    c.loop();
   }
-})
 
-const turtle = new Turtle();
-
-function animate() {
-  c.clear();
-
-  turtle.render();
-
-  c.loop();
 }
